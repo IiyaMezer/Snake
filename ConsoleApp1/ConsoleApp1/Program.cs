6e90853f7f087fad21cf28c9c50a244dd545181f
Mezer_Ilya <mr.mezer@myttk.ru>
2022-05-13 11:03:56 +0700

    Constustors

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,41 +10,13 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-
-
-            //int x1 = 1;
-            //int y1 = 3;
-            //char sym1 = '*';
-
-            //Draw(x1, y1, sym1);
-
-            //int x2 = 4;
-            //int y2 = 5;
-            //char sym2 = '#';
-            //Draw(x2, y2, sym2);     Это первая вариация
-
-            //point p1 = new point();
-            //p1.x = 1;
-            //p1.y = 3;
-            //p1.sym = '*';
-            //Draw(p1.x, p1.y, p1.sym);
-
-            //point p2 = new point();
-            //p2.x = 4;
-            //p2.y = 5;
-            //p2.sym = '#';
-            //Draw(p2.x, p2.y, p2.sym);   Вариация через классы
-
-            point p1 = new point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
+            
+            point p1 = new point(1,3,'*');
+           
             p1.Draw();
 
-            point p2 = new point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
+            point p2 = new point(4,5, '#');
+            
             p2.Draw();
 
             Console.ReadLine();
